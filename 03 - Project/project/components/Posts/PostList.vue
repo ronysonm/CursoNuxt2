8d3c77ad8518ab39c1846dfa23f733cf8fed1dfa
Ronyson Martins <ronysonm@gmail.com>
2022-09-22 20:40:27 -0300

    Finalizado o Cap 3

---
+++ 03 - Project/project/components/Posts/PostList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="featured-posts">
+    <PostPreview 
+        id="1"
+        :is-admin="isAdmin"
+        thumbnail="https://img.olhardigital.com.br/wp-content/uploads/2021/12/Code-1000x450.jpg"
+        title="First post!"
+        previewText="This is my firts post!"
+      />
+      <PostPreview 
+        id="2"
+        :is-admin="isAdmin"
+        thumbnail="https://img.olhardigital.com.br/wp-content/uploads/2021/12/Code-1000x450.jpg"
+        title="Second post!"
+        previewText="This is my second post!"
+      />
+      <PostPreview 
+        id="1"
+        :is-admin="isAdmin"
+        thumbnail="https://img.olhardigital.com.br/wp-content/uploads/2021/12/Code-1000x450.jpg"
+        title="Third post!"
+        previewText="This is my third post!"
+      />
+  </div>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+export default {
+    components: {
+        PostPreview
+    },
+    props: {
+        isAdmin: {
+            type: Boolean,
+            required: false
+        }
+    }
+}
+</script>
+
+<style>
+.featured-posts {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ 03 - Project/project/pages/admin/_postId/index.vue
@@ -0,0 +1,32 @@
+<template>
+   <div class="admin-new-post-page">
+    <section class="new-post-form">
+        <AdminPostForm  :post="loadedPost" />
+    </section>
+   </div>
+</template>
+
+<script>
+import AdminPostForm from '@/components/Admin/AdminPostForm'
+
+export default {
+    components: {
+        AdminPostForm
+    },
+    data() {
+         return {
+            loadedPost: {
+                author: 'Ronyson',
+                title: 'My great post!',
+                thumbnailLink: 'https://img.olhardigital.com.br/wp-content/uploads/2021/12/Code-1000x450.jpg',
+                content: 'This is my greatest post on the blog!'
+            }
+         }
+    },
+    layout: 'admin'
+}
+</script>
+
+<style>
+
+</style>
